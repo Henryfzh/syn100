e85b0a726c108208a17697464073a815705c1e2e
Shimo <henryfzh@outlook.com>
2024-05-30 13:50:05 -0700

    update

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-document.getElementById('ixbq').addEventListener('click', function() {
-    var dropdown = this.querySelector('.dropdown-content');
-    if (dropdown.style.display === 'block') {
-        dropdown.style.display = 'none';
-    } else {
-        dropdown.style.display = 'block';
-    }
+document.getElementById('ixbq').addEventListener('click', function(event) {
+  // Check if the clicked target is a link within the dropdown
+  if (event.target.tagName === 'A') {
+      return;  // Allow the link to be followed
+  }
+
+  var dropdown = this.querySelector('.dropdown-content');
+  if (dropdown.style.display === 'block') {
+      dropdown.style.display = 'none';
+  } else {
+      dropdown.style.display = 'block';
+  }
 });
+
 
 document.addEventListener('DOMContentLoaded', function() {
     // Add click event listeners to all card elements
